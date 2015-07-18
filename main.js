8db859d17180386f87073400f8eaa3340921d4cc
David Asabina <david@supr.nu>
2015-07-18 10:02:39 +0200

    Graph ui experiment

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-var app = require('app');
-var BrowserWindow = require('browser-window')
+var app = require('app'),
+    BrowserWindow = require('browser-window'),
+    Menu = require('menu');
 
-console.log("OTS Studio");
+var DEBUG = true;
+
+var log = function(str) {
+  if(DEBUG) { console.log(str); }
+}
+
+log("OTS Studio");
+
+//console.log(Menu);
+
+/*
+var dockMenu = Menu.buildFromTemplate([
+  { label: 'New Simulation', click: function() { console.log("sim"); } }
+]);
+app.dock.setMenu(dockMenu);
+*/
+
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  log("closing app");
+  app.quit();
+  //if (process.platform != 'darwin') app.quit();
 });
 
+app.on('enter-full-screen', function() {
+  log("full screen");
+});
+
+// just get the app ready
 app.on('ready', function() {
+  log("ready");
   mainWindow = new BrowserWindow({ width: 500, height: 300, title: 'OTS Studio'  });
 
+  mainWindow.loadUrl("file://" + __dirname + "/index.html")
+  //mainWindow.setRepresentedFilename('/etc/sim.ots');
+  //mainWindow.setDocumentEdited(true);
   mainWindow.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
